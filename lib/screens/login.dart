25a2e0a73ce0057f1d2e62256958781311126e53
Si Yujun <siuj0810@gmail.com>
2024-07-24 18:29:23 +0900

    Guest Login 구현 완료

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:simple_shopping_mall/controller/anonymous_auth_controller.dart';
 import 'package:simple_shopping_mall/controller/google_auth_controller.dart';
 import 'package:simple_shopping_mall/widgets/button/login_button.dart';
 
 class Login extends StatelessWidget {
   Login({super.key});
   final googleAuthController = Get.put(GoogleAuthController());
+  final anonymousAuthController = Get.put(AnonymousAuthController());
 
   @override
   Widget build(BuildContext context) {
@@ -51,14 +53,25 @@ class Login extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            const LoginButton(
-              logoColor: Colors.black,
-              title: 'GUEST',
-              backgroundColor: Color.fromRGBO(189, 189, 189, 1),
-              icon: Icon(
-                Icons.question_mark_rounded,
-                color: Colors.white,
-              ),
+            Obx(
+              () {
+                if (anonymousAuthController.isLoading.value) {
+                  return const CircularProgressIndicator();
+                } else {
+                  return LoginButton(
+                    logoColor: Colors.black,
+                    title: 'GUEST',
+                    backgroundColor: const Color.fromRGBO(189, 189, 189, 1),
+                    icon: const Icon(
+                      Icons.question_mark_rounded,
+                      color: Colors.white,
+                    ),
+                    onClickEvent: () {
+                      anonymousAuthController.loginWithAnonymous();
+                    },
+                  );
+                }
+              },
             ),
           ],
         ),
